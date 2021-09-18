d9a06b15ae9b69aa0e5d20828e38f0e6d337f671
zhuowei zhang <zhuoweiz10@gmail.com>
2021-09-18 17:36:29 -0400

    [zhuoweiz] basic interaction

--- src/Start.js
+++ src/Start.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Button, Typography } from '@mui/material';
+import { Button, Typography, Grid, Paper } from '@mui/material';
 
 import { getAuth, signOut, signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from "firebase/auth";
 import DrawingBoard from 'react-drawing-board';
@@ -17,6 +17,35 @@ import DrawingBoard from 'react-drawing-board';
 //   Route,
 //   Link
 // } from "react-router-dom";
+
+const GameModule = () => {
+  const [operations, setOperations] = React.useState();
+
+  return(
+    <Grid container justifyContent={'center'} alignItems={'center'}>
+      <Grid item style={{
+        width: 840,
+        backgroundColor: 'black',
+        padding:20,
+      }}>
+        <DrawingBoard
+          style={{
+            width: 800,
+            height: 500,
+          }}
+          
+          userId="user1" // identify for different players.
+          operations={operations}
+          onChange={(newOperation, afterOperation) => {
+            console.log(`TODO: send ${newOperation}`);
+            setOperations(afterOperation);
+          }}
+        />
+      </Grid>
+    </Grid>
+  )
+
+}
 
 const Start = () => {
 
@@ -39,18 +68,44 @@ const Start = () => {
     }
   });
 
-  const [operations, setOperations] = React.useState();
+  
   const [loggedIn, setLoggedIn] = React.useState(false);
   const [username, setUsername] = React.useState("");
+  const [ready, setReady] = React.useState(false);
 
   return (
-    <div>
-      Welcome to the game, you will get a hint then draw it on the whiteboard. Our server will be the judge. Try to beat your component.
+    <div style={{
+      paddingTop: 40
+    }}>
+
+      {/* <Grid> */}
+      <Grid container justifyContent={'center'} alignItems={'center'} >
+        <Grid item>
+          <Paper variant="outlined" style={{
+            width: 300,
+            padding: 16,
+          }}>
+            <Typography>
+              Welcome to the game, you will get a hint then draw it on the whiteboard. Our server will be the judge. Try to beat your component.
+            </Typography>
+          </Paper>
+        </Grid>
+      </Grid>
+      
+      
+      
 
       {
         loggedIn ? 
         <>
           <Typography>{username}</Typography> 
+          <Button
+            // disabled={ready}
+            onClick={()=>{
+              setReady(!ready);
+              // TODO send ready signal to server
+            }}
+          >{ready ? "Leave" : "Ready"}</Button>
           <Button
             onClick={()=>{
               signOut(auth).then(() => {
@@ -62,50 +117,48 @@ const Start = () => {
               });
             }}
           >Sign Out</Button>
+
+          {
+            ready ? <GameModule></GameModule> : null
+          }
+          
         </>
         :
-        <Button color='primary'
-          onClick={() => {
-            signInWithPopup(auth, provider)
-              .then((result) => {
-                // This gives you a Google Access Token. You can use it to access the Google API.
-                const credential = GoogleAuthProvider.credentialFromResult(result);
-                const token = credential.accessToken;
-                // The signed-in user info.
-                const user = result.user;
-                console.log("user: ", user);
+        <>
+          <Button color='primary'
+            onClick={() => {
+              signInWithPopup(auth, provider)
+                .then((result) => {
+                  // This gives you a Google Access Token. You can use it to access the Google API.
+                  const credential = GoogleAuthProvider.credentialFromResult(result);
+                  const token = credential.accessToken;
+                  // The signed-in user info.
+                  const user = result.user;
+                  console.log("user: ", user);
 
-                setLoggedIn(true);
-                setUsername(user.email);
+                  setLoggedIn(true);
+                  setUsername(user.email);
+                  
+                  // ...
+                }).catch((error) => {
+                  // Handle Errors here.
+                  const errorCode = error.code;
+                  const errorMessage = error.message;
+                  // The email of the user's account used.
+                  const email = error.email;
+                  // The AuthCredential type that was used.
+                  const credential = GoogleAuthProvider.credentialFromError(error);
+                  // ...
+                });
                 
-                // ...
-              }).catch((error) => {
-                // Handle Errors here.
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                // The email of the user's account used.
-                const email = error.email;
-                // The AuthCredential type that was used.
-                const credential = GoogleAuthProvider.credentialFromError(error);
-                // ...
-              });
-              
-          }}
-        >
-          LogIn
-        </Button>
+            }}
+          >
+            LogIn
+          </Button>
+
+          
+        </>
       }
-
-      
-
-      <DrawingBoard
-        userId="user1" // identify for different players.
-        operations={operations}
-        onChange={(newOperation, afterOperation) => {
-          console.log(`TODO: send ${newOperation}`);
-          setOperations(afterOperation);
-        }}
-      />
     </div>
   );
 }
